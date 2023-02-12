a9b1d06fa608e51ac2f7da4c3e01c729250b9c71
milanmaglov <maglov.milan@gmail.com>
2023-02-12 13:48:49 +0100

    Entity

---
+++ UgovorOZakupuService/UgovorOZakupuService/Entities/ParcelaEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UgovorOZakupuService.Entities
+{
+    public class ParcelaEntity
+    {
+        public ParcelaEntity()
+        {
+        }
+        [Key]
+        public Guid ParcelaID { get; set; }
+
+        public string UkupnaPovrsina { get; set; }
+
+
+
+       
+
+      
+   }
+}
---
+++ UgovorOZakupuService/UgovorOZakupuService/Entities/UgovorOZakupu.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UgovorOZakupuService.Entities
+{
+    public class UgovorOZakupu
+    {
+        public UgovorOZakupu() { }
+
+        [Key]
+        public Guid UgovorID { get; set; }
+
+        [ForeignKey("DeoParceleDTO")]
+        public Guid DeoParceleID { get; set; }
+
+        [ForeignKey("KupacDTO")]
+        public Guid KupacID { get; set; }
+
+        [ForeignKey("OvlascenoLiceDTO")]
+        public Guid OvlascenoLiceID { get; set; }
+
+        [ForeignKey("JavnoNadmetanjeDTO")]
+        public Guid JavnoNadmetanjeID { get; set; }
+
+        [ForeignKey("DokumentDTO")]
+        public Guid DokumentID { get; set; }
+
+        public DateOnly DatumUgovora { get; set; }
+
+        public int TrajanjeUgovora { get; set; }
+
+        public int TipGarancije { get; set; }
+
+    }
+}
